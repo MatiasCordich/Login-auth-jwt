d230a0a759206062fcd1448e43e5e15da4246fe4
MatiasCordich <m.scordich@gmail.com>
2022-11-06 16:28:20 -0300

    fixed validations

--- helpers/validation_schema.js
+++ helpers/validation_schema.js
@@ -3,9 +3,18 @@ const Joi = require('@hapi/joi')
 // Creo el schema para validar los campos del body en el register
 
 const schemaRegister = Joi.object({
-    name: Joi.string().min(3).max(255).required(),
-    email: Joi.string().min(6).max(255).required(),
-    password: Joi.string().min(8).max(1024).required(),
+    name: Joi.string()
+        .min(3)
+        .max(255)
+        .required(),
+    email: Joi.string()
+        .min(6)
+        .max(255)
+        .required(),
+    password: Joi.string()
+        .min(8)
+        .max(1024)
+        .required(),
 })
 
 // Creo el schema para validar los campos del body en el login
